63ef56e992c0cbaeb95569679b580ad8bff1a651
pragyanshparashar <122858764+pragyanshparashar@users.noreply.github.com>
2023-03-30 18:22:06 +0530

    className app text align centre

---
+++ src/TodoList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TodoList() {
+  return (
+    <>
+      <div className="container">
+        <div className="header">
+          <h1>Todo List</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TodoList;
