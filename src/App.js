484cc356e811312746a98441c9878825d04cb1b2
pragyanshparashar <122858764+pragyanshparashar@users.noreply.github.com>
2023-04-01 20:08:56 +0530

    map method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import TodoList from "./TodoList";
+import image from "./images/todo2.png";
 
 function App() {
   return (
     <div className="app">
+      <img className="img" src={image} height={200} width={200} />
       <TodoList />;
     </div>
   );
